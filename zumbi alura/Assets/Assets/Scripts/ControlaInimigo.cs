4d083e693976a8d9607305967040b6b219e72362
anxmarks <116958957+anxmarks@users.noreply.github.com>
2023-02-13 16:00:40 -0300

    modulo3

--- zumbi alura/Assets/Assets/Scripts/ControlaInimigo.cs
+++ zumbi alura/Assets/Assets/Scripts/ControlaInimigo.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class ControlaInimigo : MonoBehaviour
 {
+
+    public GameObject Jogador;
+    public float Velocidade = 5;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +17,21 @@ public class ControlaInimigo : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void FixedUpdate() 
+    {
+       
+        float distancia = Vector3.Distance(transform.position, Jogador.transform.position);
+
+        if(distancia > 2.5)
+        {
+             Vector3 direcao = Jogador.transform.position - transform.position;
+             GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position + direcao.normalized * Velocidade * Time.deltaTime);
+
+            Quaternion novaRotacao = Quaternion.LookRotation(direcao);
+             GetComponent<Rigidbody>().MoveRotation(novaRotacao);
+
+        }
     }
 }
